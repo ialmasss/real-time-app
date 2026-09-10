e0a335c011a1d46b0ca8285d9efb53c7acdca54a
Almas <almass11dd@gmail.com>
2026-09-10 09:26:36 +0500

    adding websocket

---
+++ src/main/java/com/example/realtimeapp/controller/ChatWebSocketController.java
@@ -0,0 +1,53 @@
+package com.example.realtimeapp.controller;
+
+import com.example.realtimeapp.dto.ChatMessageRequest;
+import com.example.realtimeapp.dto.MessageDto;
+import com.example.realtimeapp.model.Message;
+import com.example.realtimeapp.model.User;
+import com.example.realtimeapp.model.Chat;
+import com.example.realtimeapp.repository.ChatRepository;
+import com.example.realtimeapp.repository.MessageRepository;
+import com.example.realtimeapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatWebSocketController {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat.send")
+    public void sendMessage(ChatMessageRequest request){
+
+        Chat chat = chatRepository.findById(request.getChatId())
+                .orElseThrow(() -> new RuntimeException("Chat is not found"));
+
+        User sender = userRepository.findById(request.getSenderId())
+                .orElseThrow(() -> new RuntimeException(("User is not found")));
+
+        Message message = new Message();
+        message.setContent(request.getContent());
+        message.setChat(chat);
+        message.setSender(sender);
+
+        Message savedMessage = messageRepository.save(message);
+
+        MessageDto messageDto = new MessageDto(savedMessage);
+
+        messagingTemplate.convertAndSend("/topic/chat/" + chat.getId(), messageDto);
+    }
+
+
+}
---
+++ src/main/java/com/example/realtimeapp/dto/ChatMessageRequest.java
@@ -0,0 +1,17 @@
+package com.example.realtimeapp.dto;
+
+public class ChatMessageRequest {
+    private Long chatId;
+    private Long senderId;
+    private String content;
+
+    public Long getChatId(){ return chatId;}
+    public void setChatId(Long chatId) {this.chatId = chatId;}
+
+    public Long getSenderId(){ return senderId;}
+    public void setSenderId(Long senderId){this.senderId = senderId;}
+
+    public String getContent(){ return content;}
+    public void setContent(String content){this.content = content;}
+
+}
